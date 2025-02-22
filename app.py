0a97238a5c00cbe75d5c7b1fa6899150746ada63
wangsop <sophia.riverfront@gmail.com>
2025-02-21 23:36:40 -0500

    projects and individual project pages

--- app.py
+++ app.py
@@ -1,20 +1,46 @@
 import sqlite3
 from flask import Flask, render_template
+from werkzeug.exceptions import abort
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     conn = get_db_connection()
-    projects = conn.execute('SELECT * FROM projects').fetchall()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
-    return render_template('index.html', projects=projects)
+    return render_template('index.html', posts=posts)
 
 @app.route('/login/')
 def logIn():
     return 'This is a placeholder for the sign in/make new account screen'
 
+@app.route('/projects/')
+def projects():
+    account_id: int = get_account_id()
+    conn = get_db_connection()
+    projects = conn.execute('SELECT * FROM projects WHERE accountID = ?',(account_id,)).fetchall()
+    conn.close()
+    return render_template('projects.html', projects=projects)
+
+@app.route('/<int:project_id>')
+def project(project_id):
+    project = get_project(project_id)
+    return render_template('project.html', project=project)
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
+
+def get_project(project_id):
+    conn = get_db_connection()
+    project = conn.execute('SELECT * FROM projects WHERE id = ?',(project_id,)).fetchone()
+    conn.close()
+    if project is None:
+        abort(404)
+    return project
+
+def get_account_id():
+    #placeholder until I figure out how to fetch account id from login
+    return 1
--- init_db.py
+++ init_db.py
@@ -8,12 +8,16 @@ with open('schema.sql') as f:
 
 cur = connection.cursor()
 
-cur.execute("INSERT INTO projects (title, accountID, stack) VALUES (?, ?, ?)",
-            ('Manhattan Project', 1, 'Something secure')
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('New firewall attack method', 'hacker has found something new about firewalls blah blah ....')
             )
 
 cur.execute("INSERT INTO projects (title, accountID, stack) VALUES (?, ?, ?)",
-            ('Some Project', 1, 'Something less secure')
+            ('Google Drive', 1, 'Something secure')
+            )
+
+cur.execute("INSERT INTO projects (title, accountID, stack) VALUES (?, ?, ?)",
+            ('Gmail', 1, 'Something also secure')
             )
 
 connection.commit()
